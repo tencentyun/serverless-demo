dfb14bff511b803cae85c74682247967936d26d1
dorasang <sangliu111@126.com>
2020-12-09 15:35:50 +0800

    update dir name

--- Python2.7-HelloWorld/src/index.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf8 -*-
-print('Start Hello World function')
-def main_handler(event, context):
-    print('Hello World')
-    if 'key1' in event.keys():
-        print("value1 = " + event['key1'])
-    if 'key2' in event.keys():
-        print("value2 = " + event['key2'])
-    return "hello from scf"  #return
