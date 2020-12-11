960513fa4a0f2af6c39efc207a140c3f877c6f07
dorasang <sangliu111@126.com>
2020-12-11 17:14:45 +0800

    update

--- Python2.7-CLSSCFCOS/src/index.py
+++ Python2.7-CLSSCFCOS/src/index.py
@@ -1,11 +1,5 @@
-# -*- coding=utf-8
-
-#####----------------------------------------------------------------#####
-#####                                                                #####
-#####   使用教程/readme:                                              #####
-#####   https://cloud.tencent.com/document/product/583/307**         #####
-#####                                                                #####
-#####----------------------------------------------------------------#####
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import os
 import logging
@@ -22,20 +16,23 @@ from qcloud_cos_v5 import CosServiceError
 from qcloud_cos_v5 import CosClientError
 from StringIO import StringIO
 
-
 # Setting user properties, including secret_id, secret_key, region, bucket
 # 设置用户属性, 包括secret_id, secret_key, region，bucket
-appid = '1251762227'  # Please replace with your APPID. 请替换为您的 APPID
-secret_id = u'*********'  # Please replace with your SecretId. 请替换为您的 SecretId
-secret_key = u'********'  # Please replace with your SecretKey. 请替换为您的 SecretKey
+appid = 'xxxxx'  # Please replace with your APPID. 请替换为您的 APPID
+secret_id = u'xxxx'  # Please replace with your SecretId. 请替换为您的 SecretId
+secret_key = u'xxxx'  # Please replace with your SecretKey. 请替换为您的 SecretKey
 region = 'ap-guangzhou'  # Please replace with your region. 替换为用户的region
 token = ''  # To use the temporary key, you need to pass in the Token. The default is empty. 使用临时秘钥需要传入Token，默认为空,可不填
-bucket_upload = "Tabor-ckafka-scf-cos-1251762227"  # Please replace with your COS bucket. 替换为需要写入的COS Bucket
+bucket_upload = "xxx-xxx"  # Please replace with your COS bucket. 替换为需要写入的COS Bucket
 
 # Getting configuration object. 获取配置对象
-config = CosConfig(Region=region, Secret_id=secret_id, Secret_key=secret_key, Token=token)
+config = CosConfig(Region=region,
+                   Secret_id=secret_id,
+                   Secret_key=secret_key,
+                   Token=token)
 client = CosS3Client(config)
 logger = logging.getLogger()
+
 
 # Generating file name. 生成写入文件名
 def object_key_generater():
@@ -44,26 +41,25 @@ def object_key_generater():
     time = datetime.datetime.now(tz).strftime("%Y-%m-%d-%H:%M:%S")
     random_name = random.randint(1, 200)
     dir_name = datetime.datetime.now(tz).strftime("%Y-%m-%d-%H")
-    file_name = format(time) + '-' + str(random_name) +'.txt'
-    object_key = '{}/{}'.format( dir_name,file_name)
+    file_name = format(time) + '-' + str(random_name) + '.txt'
+    object_key = '{}/{}'.format(dir_name, file_name)
     return object_key
+
 
 # Check if the file already exists. 检查文件是否已存在
 def check_cos_file(key):
     try:
-        resp = client.head_object(
-            Bucket=bucket_upload,
-            Key=key
-        )
-        logger.info("check_cos_file of resp is [%s]"% resp)
+        resp = client.head_object(Bucket=bucket_upload, Key=key)
+        logger.info("check_cos_file of resp is [%s]" % resp)
         return True
     except CosServiceError, e:
         # print("head error")
         # if e['code'] == "NoSuchResource":
-        logger.info("e is [%s]"% e)
-        if e.get_error_code()== "NoSuchResource":
-            logger.info("check_cos_file is [%s]"% e.get_error_code())
+        logger.info("e is [%s]" % e)
+        if e.get_error_code() == "NoSuchResource":
+            logger.info("check_cos_file is [%s]" % e.get_error_code())
             return False
+
 
 # Deleting local file. 删除本地文件
 def delete_local_file(src):
@@ -82,19 +78,19 @@ def delete_local_file(src):
         except:
             pass
 
+
 # Uploading file to COS. 上传文件到COS
 def upload_loacal_file(loacalpath):
     logger.info("Start to upload")
     if os.path.isfile(loacalpath):
         logger.info("local_filename is [%s]" % loacalpath)
         key = object_key_generater()
-        while check_cos_file(key) == True:
+        if check_cos_file(key) == True:
             key = object_key_generater()
         logger.info("cos_object_name is [%s]" % key)
-        response = client.put_object_from_local_file(
-            Bucket=bucket_upload,
-            LocalFilePath=loacalpath,
-            Key=key)
+        response = client.put_object_from_local_file(Bucket=bucket_upload,
+                                                     LocalFilePath=loacalpath,
+                                                     Key=key)
         logger.info("upload result is [%s]" % response)
         delete_local_file(str(loacalpath))
         return True
@@ -102,17 +98,34 @@ def upload_loacal_file(loacalpath):
         logger.info("Upload fail")
         return False
 
+
+def write_to_file(local_path, data):
+    with open(local_path, "a") as f:
+        logger.info("data is [%s]" % data)
+        f.write(data)
+        f.write('\r\n')
+
+
 def main_handler(event, context):
     logger.info("start main handler")
     local_path = '/tmp/local_file.txt'
-    data = ""
-    event = json.loads(gzip.GzipFile(fileobj=StringIO(event['clslogs']['data'].decode('base64'))).read())
-    clsdata = json.dumps(event, indent=4, sort_keys=True)
-    with open(local_path, "a") as f:
-        data = clsdata.decode("unicode-escape")
-        # logger.info("data is [%s]" % data)
-        .write(data)
-        .write('\r\n')
+    event = json.loads(
+        gzip.GzipFile(fileobj=StringIO(event['clslogs']['data'].decode(
+            'base64'))).read())
+    # clsdata = json.dumps(event, indent=4, sort_keys=True)
+    logger.info("topic_id is %s" % event['topic_id'])
+    logger.info("topic_name is %s" % event['topic_name'])
+
+    # 处理日志并，写入临时文件
+    for record in event['records']:
+        # 处理数据再写入
+        data = record['content'].replace('value2', "value2_replaced")
+        try:
+            write_to_file(local_path, data)
+        except:
+            logger.error("Error occured when writing")
+
+    # 上传处理后的日志文件
     if upload_loacal_file(local_path) == True:
         return "write success"
     else:
--- Python2.7-CLSSCFETL/src/index.py
+++ Python2.7-CLSSCFETL/src/index.py
@@ -1,94 +1,60 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
- 
-import time
-import logging
-import os
+# -*- coding: utf8 -*-
 import base64
 import json
 import gzip
 import urllib
-from kafka import KafkaProducer
-from kafka.errors import KafkaError
+import os
+import json
+import datetime
+import time
+import logging
 from StringIO import StringIO
 
-logger = logging.getLogger('kafka')
-logger.setLevel(logging.INFO)
- 
-class ClsToKafka(object):
-    """
-    CLS 消息投递 kafka
-    """
+# 日志设置
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG) # 日志等级        
 
-    def __init__(self, host, **kwargs):
-        self.host = host
+# 定义需要屏蔽的字符串, 以字典形式，如密码123456
+Clean_Word = {""}
 
-        self.producer = KafkaProducer(
-            bootstrap_servers = [self.host], 
-            retries = 10,
-            max_in_flight_requests_per_connection = 1, 
-            request_timeout_ms = 30000,
-            max_block_ms = 60000,
-            **kwargs
-        )
+# 定制日志清洗功能,将字符串中的敏感信息替换成***
+def cleanData(data):
+    try:
+        for word in Clean_Word:
+            data = record['content'].replace(word, "***")
+        return data
+    except:
+        logger.error("Error occured when cleanning data")
+        raise
 
-    def send(self, topic, records):
-        """
-        异步生产 kafka 消息
-        """
-
-        global count
-        count = 0
-        def on_send_success(record_metadata):
-            global count
-            count = count +1
-
-        def on_send_error(excp):
-            logger.error('failed to send message', exc_info = excp)
-
-        s_time = time.time()
+# 打印处理结果
+def printData(records):
+    for record in records:
+        # 处理数据再写入
+        data = cleanData(data)
+        # 打印结果
         try:
-            for record in records:
-                key = ""
-                # 当 key 为 "" 或者为 "None" 时，要传入key=None，这样python kafka库会随机选取一个partition写入消息
-                if key == "" or key == "None":
-                    key = None
-                value = data
-
-                # 也可以对消息进行处理后再转存
-                #value = deal_message(data)
-
-                self.producer.send(topic, key = key, value = value).add_callback(on_send_success).add_errback(on_send_error)
-
-            # block until all async messages are sent
-            self.producer.flush()
-        except KafkaError as e:
-            return e
-        finally:
-            if self.producer is not None:
-                self.producer.close()
-        e_time = time.time()
-
-        return "{} messages delivered in {}s".format(count, e_time - s_time)
-
-# 这里可以对消息进行处理后返回
-def deal_message(message):
-    return message
+            print(data)
+        except:
+            logger.error("Error occured when writing")
+            raise
 
 def main_handler(event, context):
-    kafka_address = os.getenv("kafka_address")
-    kafka_topic_name = os.getenv("kafka_topic_name")
-  
-    kafka_to_kafka = ClsToKafka(
-        kafka_address
-        #security_protocol = "SASL_PLAINTEXT",
-        #sasl_mechanism = "PLAIN",
-        #sasl_plain_username = "ckafka-80o10xxx#lkoxx",
-        #sasl_plain_password = "ccllxxxx",
-        #api_version=(0, 10, 2)
-    )
+    logger.debug("start main_handler")
     event = json.loads(gzip.GzipFile(fileobj=StringIO(event['clslogs']['data'].decode('base64'))).read())
     data = json.dumps(event, indent=4, sort_keys=True)
-    ret = kafka_to_kafka.send(kafka_topic_name,data)
-    logger.info(ret)
-    return ret
+    # CLS消息的数量
+    num = len(data['records'])
+    logger.debug("the length of msg body is [%s]" % num)    
+    logger.debug("start writing to es")
+    printData(data['records'])
+
+    # 直接处理日志并写入临时文件方法
+    # for record in event['records']:
+    #     data = record['content'].replace('value2', "value2_replaced")
+    #     try:
+    #         write_to_file(local_path, data)
+    #     except:
+    #         logger.error("Error occured when writing")
+
+    return 'success'
