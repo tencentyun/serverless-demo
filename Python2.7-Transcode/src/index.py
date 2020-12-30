1e7d3f39a62c00969f706bf0f61a385ed6892370
dorasang <sangliu111@126.com>
2020-12-30 21:05:37 +0800

    Update index.py

--- Python2.7-Transcode/src/index.py
+++ Python2.7-Transcode/src/index.py
@@ -1,33 +1,33 @@
-
-
+#!/usr/bin/env python
+# coding:utf-8
 #####----------------------------------------------------------------#####
 #####                                                                #####
 #####   使用教程/readme:                                              #####
 #####   https://cloud.tencent.com/document/product/583/47071         #####
 #####                                                                #####
 #####----------------------------------------------------------------#####
-
 import os
-import subprocess
 from qcloud_cos_v5 import CosConfig
 from qcloud_cos_v5 import CosS3Client
 import logging
-import sys
-import re
-import math
 import requests
+import sys
 
 region = os.environ.get('region')
 target_bucket = os.environ.get('target_bucket')
 target_path = os.environ.get('target_path')
 
+#指定ffmpeg默认运行的路径
+path = os.getenv('PATH')
+os.environ['PATH']=path+":/tmp"
+
 #ffmpeg命令
 #视频压缩
-video_press = '/tmp/ffmpeg  -i %s -r 10 -b:a 32k %s'
+video_press = 'ffmpeg  -i %s -r 10 -b:a 32k %s'
 #视频截取 
-video_cut = '/tmp/ffmpeg -ss 00:00:00 -t 00:00:10 -i %s -vcodec copy -acodec copy %s'
+video_cut = 'ffmpeg -ss 00:00:00 -t 00:00:10 -i %s -vcodec copy -acodec copy %s'
 #获取视频时长
-video_length = '/tmp/ffmpeg -i %s 2>&1 | grep "Duration"'
+video_length = 'ffmpeg -i %s 2>&1 | grep "Duration"'
 
 #控制log输出级别
 logging.basicConfig(level=logging.INFO, stream=sys.stdout)
@@ -38,7 +38,7 @@ logger.setLevel(level=logging.INFO)
 with open("/var/user/ffmpeg", "rb") as rf:
     with open("/tmp/ffmpeg", "wb") as wf:
         wf.write(rf.read())
-subprocess.run('chmod 755 /tmp/ffmpeg', shell=True)
+os.popen('chmod 755 /tmp/ffmpeg')
 
 
 #删除本地文件
@@ -60,11 +60,6 @@ def delete_local_file(src):
 
 def main_handler(event, context):
     logger.info("start main handler")
-    #使用临时秘钥初始化COS Client
-    secret_id = os.environ.get('TENCENTCLOUD_SECRETID')      
-    secret_key = os.environ.get('TENCENTCLOUD_SECRETKEY')    
-    token = os.environ.get('TENCENTCLOUD_SESSIONTOKEN') 
-    cosClient = CosS3Client(CosConfig(Region=region, SecretId=secret_id, SecretKey=secret_key,Token=token))
     #判断请求是否从API网关传递,通过网关传递源文件下载地址，在body中获取
     if "body" in event.keys():
         download_path = event['body']
@@ -80,12 +75,18 @@ def main_handler(event, context):
         logger.info('cos download_path: '+ download_path)
     else:
         return {"code": 410, "errorMsg": "event does not come from COS or APIGW"}
+    #使用临时秘钥初始化COS Client
+    secret_id = os.environ.get('TENCENTCLOUD_SECRETID')      
+    secret_key = os.environ.get('TENCENTCLOUD_SECRETKEY')    
+    token = os.environ.get('TENCENTCLOUD_SESSIONTOKEN') 
+    cosClient = CosS3Client(CosConfig(Region=region, SecretId=secret_id, SecretKey=secret_key,Token=token))
 
     logger.info('key: '+ key)
     upload_path = '/tmp/new-'+ key.split('/')[-1]
     logger.info('upload_path: '+ upload_path)
     # 执行ffmpeg命令，从下载地址读流的方式进行转码
-    child = subprocess.run( video_press %(download_path, upload_path), stdout=subprocess.PIPE, stderr=subprocess.PIPE, close_fds=True, shell=True)
+    ret = os.popen( video_press %(download_path, upload_path) ).read()
+    print(ret)
     # 上传视频，可自定义上传路径
     cosClient.put_object_from_local_file(
         Bucket=target_bucket,
