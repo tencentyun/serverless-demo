63e1e77a208e311def79197b519f1f89395d5dc1
ciryu <helloyucx@163.com>
2021-05-28 16:22:10 +0800

    update version 1.0.2

--- Python3.6-SingleAudioRecorder/src/index.py
+++ Python3.6-SingleAudioRecorder/src/index.py
@@ -23,6 +23,7 @@ cmd_audio_2mp3 = cmd_path_ffmpeg + ' -i %s -f mp3 -vn %s '
 # 录制命令，开启单流录制
 cmd_path = './recordClient/recorderClient'
 cmd_record = cmd_path + ' --demoSences %d --sdkAppId %s --roomId %d  --userId %s --userSig %s --recDir=%s --recFiles=%d --timeout=%d'
+cmd_record_str_room = cmd_path + ' --demoSences %d --sdkAppId %s --roomId %s  --userId %s --userSig %s --recDir=%s --recFiles=%d --timeout=%d --strRoom=%d'
 
 # 日志配置
 logging.basicConfig(level=logging.INFO, stream=sys.stdout)
@@ -75,16 +76,18 @@ def delete_local_file(src):
 # 可能存在录制失败的中间文件，都做一个统计
 # 输出flv音频文件、录制失败的中间文件、总文件数
 def check_record_files(path):
-    record_files = os.listdir(path)  # 得到文件夹下的所有文件名称
+    record_files = []
     flv_files = []
     failed_files = []
-    for file in record_files:  # 遍历文件夹
-        if not os.path.isdir(file):  # 判断是否是文件夹
-            path_ = path + "/" + file
-            if path_.endswith(".flv"):
-                flv_files.append(path_)
-            else:
-                failed_files.append(path_)
+    if os.path.exists(path):
+        record_files = os.listdir(path)  # 得到文件夹下的所有文件名称
+        for file in record_files:  # 遍历文件夹
+            if not os.path.isdir(file):  # 判断是否是文件夹
+                path_ = path + "/" + file
+                if path_.endswith(".flv"):
+                    flv_files.append(path_)
+                else:
+                    failed_files.append(path_)
     logger.info("A total of %d audio files were generated. %d successfully recorded flv files, %d failed files." % (
     len(record_files), len(flv_files), len(failed_files)))
     return flv_files, failed_files, len(record_files)
@@ -117,7 +120,7 @@ def get_anchor_id(path):
 # 回调逻辑。可自定义回调逻辑
 # 存在录制失败，返回录制失败的主播和文件
 # 存在格式转换失败，返回转换失败的FLV文件
-def callback(url, sdk_app_id, room_id, user_id, cos_files, convert_failed_files, failed_files):
+def callback(url, record_failed, sdk_app_id, room_id, user_id, cos_files, convert_failed_files, failed_files):
     if len(url) <= 0:
         print("callback url is empty, no need to callback.")
         return
@@ -140,8 +143,37 @@ def callback(url, sdk_app_id, room_id, user_id, cos_files, convert_failed_files,
         data['Status'] = "fail"
         data['Message'] = message
     else:
-        data['Status'] = "success"
-        data['Files'] = cos_files
+        # 如果失败flag为True，录制出现异常，返回失败
+        if record_failed:
+            data['Status'] = "fail"
+            data['Message'] = "audio record throw exception, please check from cloud function."
+        else:
+            if len(cos_files) <= 0:
+                # 如果没有录制并转换成功文件，返回失败
+                data['Status'] = "fail"
+                data['Message'] = "audio record failed. No audio record files."
+            else:
+                # 录制成功，等待超时和未开始录制都会按照成功返回
+                data['Status'] = "success"
+                data['Files'] = cos_files
+
+    response = requests.post(url, json=data)
+    print("callback response:", response.text.encode('utf8'))
+
+
+# 回调逻辑。
+def callback_msg(url, sdk_app_id, room_id, user_id, status, message):
+    if len(url) <= 0:
+        print("callback url is empty, no need to callback.")
+        return
+
+    data = {
+        'SdkAppId': sdk_app_id,
+        'RoomId': room_id,
+        'UserId': user_id,
+        'Status': status,
+        'Message': message
+    }
 
     response = requests.post(url, json=data)
     print("callback response:", response.text.encode('utf8'))
@@ -206,16 +238,38 @@ def main_handler(event, context):
     # 也无需设置太大，会导致等待很长时间
     timeout = 300000
     sdk_app_id = trtc_param_['sdkAppId']
-    room_id = trtc_param_['roomId']
     user_id = trtc_param_['userId']
     user_sig = trtc_param_['userSig']
     callback_url = trtc_param_['callback']
 
-    # step2 开始进房，开启单流录制
-    try:
+    record_failed = False  # 失败flag
+
+    room_id = None
+    is_str_room = False
+
+    # 校验， roomId和strRoomId必传一个
+    if not trtc_param_.get('strRoomId') and not trtc_param_.get('roomId'):
+        status = "fail"
+        message = "record and upload failed. RoomId is empty"
+        callback_msg(callback_url, sdk_app_id, room_id, user_id, status, message)
+        return "record and upload failed. RoomId is empty"
+
+    if trtc_param_.get('strRoomId') and not trtc_param_.get('roomId'):
+        is_str_room = True
+
+    if not is_str_room:
+        room_id = trtc_param_['roomId']
         command = cmd_record % (
             scene_single_record, sdk_app_id, room_id, user_id, user_sig, record_path, receive_file_type, timeout)
-        print("exec record command", command)
+    else:
+        room_id = trtc_param_['strRoomId']
+        command = cmd_record_str_room % (
+            scene_single_record, sdk_app_id, room_id, user_id, user_sig, record_path, receive_file_type, timeout, 1)
+
+    print("exec record command: ", command)
+
+    # step2 开始进房，开启单流录制
+    try:
         ret = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, check=True)
         if ret.returncode == 0:
             print('single stream record finished.')
@@ -223,6 +277,7 @@ def main_handler(event, context):
         else:
             print("single stream record failed, err code:", ret.returncode)
     except Exception as e:
+        record_failed = True
         logging.exception(e)
         pass
 
@@ -263,15 +318,19 @@ def main_handler(event, context):
             delete_local_file(flv_file)
             delete_local_file(mp3_file)
         except Exception as e:
+            record_failed = True
             logging.exception(e)
             continue
 
     # 清理工作目录和日志目录
-    logger.info("clear work dir and log dir...")
-    delete_local_file(record_path)
-    delete_local_file(upload_path)
-    delete_local_file("/tmp")
+    try:
+        logger.info("clear work dir and log dir...")
+        delete_local_file(record_path)
+        delete_local_file(upload_path)
+        delete_local_file("/tmp")
+    except:
+        pass
 
-    callback(callback_url, sdk_app_id, room_id, user_id, cos_files, convert_failed_files, failed_files)
+    callback(callback_url, record_failed, sdk_app_id, room_id, user_id, cos_files, convert_failed_files, failed_files)
 
-    return "record and upload success."
+    return "record and upload finished."
