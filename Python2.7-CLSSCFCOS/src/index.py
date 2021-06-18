a980e7d5a9a41ae9cf3b6e3f0ef197765a728400
fuwenzheng <fuwenzheng@tencent.com>
2021-06-18 17:06:05 +0800

    update Python2.7-CLSSCFCOS

--- Python2.7-CLSSCFCOS/src/index.py
+++ Python2.7-CLSSCFCOS/src/index.py
@@ -1,132 +1,164 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 import os
 import logging
 import datetime
-import random
-import pytz
-import base64
-import json
 import gzip
-import urllib
+import json
+import uuid
+from StringIO import StringIO
+import pytz
 from qcloud_cos_v5 import CosConfig
 from qcloud_cos_v5 import CosS3Client
-from qcloud_cos_v5 import CosServiceError
-from qcloud_cos_v5 import CosClientError
-from StringIO import StringIO
 
-# Setting user properties, including secret_id, secret_key, region, bucket
-# 设置用户属性, 包括secret_id, secret_key, region，bucket
-appid = 'xxxxx'  # Please replace with your APPID. 请替换为您的 APPID
-secret_id = u'xxxx'  # Please replace with your SecretId. 请替换为您的 SecretId
-secret_key = u'xxxx'  # Please replace with your SecretKey. 请替换为您的 SecretKey
-region = 'ap-guangzhou'  # Please replace with your region. 替换为用户的region
-token = ''  # To use the temporary key, you need to pass in the Token. The default is empty. 使用临时秘钥需要传入Token，默认为空,可不填
-bucket_upload = "xxx-xxx"  # Please replace with your COS bucket. 替换为需要写入的COS Bucket
+COMPRESSTYPE_GZIP = 'gzip'
+COMPRESSTYPE_GZIP_SUFFIX = '.gz'
+COMPRESSTYPE_LZOP = 'lzop'
+COMPRESSTYPE_LZOP_SUFFIX = '.lzo'
+COMPRESSTYPE_SNAPPY = 'snappy'
+COMPRESSTYPE_SNAPPY_SUFFIX = '.snappy'
 
-# Getting configuration object. 获取配置对象
-config = CosConfig(Region=region,
-                   Secret_id=secret_id,
-                   Secret_key=secret_key,
-                   Token=token)
-client = CosS3Client(config)
 logger = logging.getLogger()
 
 
-# Generating file name. 生成写入文件名
-def object_key_generater():
-    logger.info("start to generate key name")
-    tz = pytz.timezone('Asia/Shanghai')
-    time = datetime.datetime.now(tz).strftime("%Y-%m-%d-%H:%M:%S")
-    random_name = random.randint(1, 200)
-    dir_name = datetime.datetime.now(tz).strftime("%Y-%m-%d-%H")
-    file_name = format(time) + '-' + str(random_name) + '.txt'
-    object_key = '{}/{}'.format(dir_name, file_name)
-    return object_key
+# object_key_generater. 生成文件名
+def object_key_generater(prefix, strftime, compress_type):
+    time_zone = pytz.timezone('Asia/Shanghai')
+    timestr = datetime.datetime.now(time_zone).strftime(strftime)
+    random = str(uuid.uuid4())
+
+    # 根据压缩类型得到文件后缀
+    file_suffix = ''
+    if compress_type == COMPRESSTYPE_GZIP:
+        file_suffix = COMPRESSTYPE_GZIP_SUFFIX
+    elif compress_type == COMPRESSTYPE_LZOP:
+        file_suffix = COMPRESSTYPE_LZOP_SUFFIX
+    elif compress_type == COMPRESSTYPE_SNAPPY:
+        file_suffix = COMPRESSTYPE_SNAPPY_SUFFIX
+
+    obj_path = ''
+    if prefix is not None:
+        obj_path = os.path.join(
+            prefix,
+            '{}_{}{}'.format(timestr, random, file_suffix)
+            )
+    else:
+        obj_path = '{}_{}{}'.format(timestr, random, file_suffix)
+    return obj_path
 
 
-# Check if the file already exists. 检查文件是否已存在
-def check_cos_file(key):
-    try:
-        resp = client.head_object(Bucket=bucket_upload, Key=key)
-        logger.info("check_cos_file of resp is [%s]" % resp)
-        return True
-    except CosServiceError, e:
-        # print("head error")
-        # if e['code'] == "NoSuchResource":
-        logger.info("e is [%s]" % e)
-        if e.get_error_code() == "NoSuchResource":
-            logger.info("check_cos_file is [%s]" % e.get_error_code())
-            return False
-
-
-# Deleting local file. 删除本地文件
 def delete_local_file(src):
-    logger.info("delete files and folders")
+    """Deleting local file. 删除本地文件"""
+
+    logger.info("delete " + src)
     if os.path.isfile(src):
         try:
             os.remove(src)
-        except:
-            pass
+        except OSError as ex:
+            print(ex)
+        
     elif os.path.isdir(src):
         for item in os.listdir(src):
             itemsrc = os.path.join(src, item)
             delete_local_file(itemsrc)
         try:
-            os.rmdir(src)
-        except:
-            pass
+            # /tmp 目录不能删
+            if src != '/tmp':
+                os.rmdir(src)
+        except OSError as ex:
+            print(ex)
 
 
 # Uploading file to COS. 上传文件到COS
-def upload_loacal_file(loacalpath):
+def upload_loacal_file(client, bucket_upload, loacalpath, obj_path):
     logger.info("Start to upload")
     if os.path.isfile(loacalpath):
-        logger.info("local_filename is [%s]" % loacalpath)
-        key = object_key_generater()
-        if check_cos_file(key) == True:
-            key = object_key_generater()
-        logger.info("cos_object_name is [%s]" % key)
+        logger.info("local_filename is [%s]", loacalpath)
+        logger.info("cos_object_name is [%s]", obj_path)
         response = client.put_object_from_local_file(Bucket=bucket_upload,
                                                      LocalFilePath=loacalpath,
-                                                     Key=key)
-        logger.info("upload result is [%s]" % response)
-        delete_local_file(str(loacalpath))
+                                                     Key=obj_path)
+        logger.info("upload result is [%s]", response)
         return True
     else:
-        logger.info("Upload fail")
+        logger.info("not local file ")
         return False
 
 
-def write_to_file(local_path, data):
-    with open(local_path, "a") as f:
-        logger.info("data is [%s]" % data)
-        f.write(data)
-        f.write('\r\n')
+def write_to_file(compress_type, local_path, data):
+    if not compress_type:
+        with open(local_path, "a") as f:
+            f.write(data)
+    elif compress_type == COMPRESSTYPE_GZIP:
+        tmp_path = "/tmp/gziptmp"
+        with open(tmp_path, "a") as f:
+            f.write(data)
+        bashcmd = './{} {}'.format('gzip', tmp_path)
+        os.system(bashcmd)
+        # gzip 会自动添加.gz后缀
+        os.rename(tmp_path + COMPRESSTYPE_GZIP_SUFFIX, local_path)
+    elif compress_type == COMPRESSTYPE_LZOP:
+        tmp_path = "/tmp/lzoptmp"
+        with open(tmp_path, "a") as f:
+            f.write(data)
+        bashcmd = './{} {} -o {}'.format('lzop', tmp_path, local_path)
+        os.system(bashcmd)
+    elif compress_type == COMPRESSTYPE_SNAPPY:
+        tmp_path = "/tmp/snappytmp"
+        with open(tmp_path, "a") as f:
+            f.write(data)
+        bashcmd = './{} -t snappy-java {}'.format('snzip', tmp_path)
+        os.system(bashcmd)
+        # snzip 会自动添加.snappy后缀
+        os.rename(tmp_path + COMPRESSTYPE_SNAPPY_SUFFIX, local_path)
 
 
 def main_handler(event, context):
-    logger.info("start main handler")
-    local_path = '/tmp/local_file.txt'
+    # logger.info("context is %s", context)
+    local_path = '/tmp/clstocostmpfile'
+    
+    # 从环境变量中获取参数
+    secret_id = os.getenv('TENCENTCLOUD_SECRETID')
+    secret_key = os.getenv('TENCENTCLOUD_SECRETKEY')
+    region = os.getenv('TENCENTCLOUD_REGION')
+    token = os.getenv('TENCENTCLOUD_SESSIONTOKEN')
+    bucket_upload = os.getenv('CLSTOCOSUPLOADOBJBUKET')
+    obj_path_prefix = os.getenv('CLSTOCOSUPLOADOBJPREFIX')
+    strftime = os.getenv('CLSTOCOSUPLOADOBJSTRFTIME')
+    compress_type = os.getenv('CLSTOCOSUPLOADOBJCOMPRESS')
+
+    # 上传路径bucket_upload/obj_path_prefix/strftime_uuid.compress_type
+    obj_path = object_key_generater(obj_path_prefix, strftime, compress_type)
+    logger.info("cos_object_name will be [%s]", obj_path)
+    config = CosConfig(
+        Region=region,
+        Secret_id=secret_id,
+        Secret_key=secret_key,
+        Token=token
+        )
+    client = CosS3Client(config)
     event = json.loads(
         gzip.GzipFile(fileobj=StringIO(event['clslogs']['data'].decode(
             'base64'))).read())
-    # clsdata = json.dumps(event, indent=4, sort_keys=True)
-    logger.info("topic_id is %s" % event['topic_id'])
-    logger.info("topic_name is %s" % event['topic_name'])
-
-    # 处理日志并，写入临时文件
+    data = ''
     for record in event['records']:
-        # 处理数据再写入
-        data = record['content'].replace('value2', "value2_replaced")
-        try:
-            write_to_file(local_path, data)
-        except:
-            logger.error("Error occured when writing")
+        data = data + record['content'].replace('value2', "value2_replaced") + "\r\n"
+    try:
+        write_to_file(compress_type, local_path, data)
+    except OSError as ex:
+        logger.error("Error occured when writing %s", ex)
+        # 清理临时目录
+        delete_local_file('/tmp')
+        raise ex
 
     # 上传处理后的日志文件
-    if upload_loacal_file(local_path) == True:
-        return "write success"
-    else:
-        return "write fail"
+    result = "ok"
+    if upload_loacal_file(
+        client, bucket_upload,
+        local_path, obj_path
+        ) is False:
+        result = "failed"
+
+    # 清理临时目录
+    delete_local_file('/tmp')
+    return result
