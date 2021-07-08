6ab620bcf3dd6e1ef461159ce8cc684cfceab421
kangding <kangding@tencent.com>
2021-07-08 20:04:19 +0800

    ADD:优化ckafka生产者

--- Python2.7-KafkaToKafkaConsumer/src/index.py
+++ Python2.7-KafkaToKafkaConsumer/src/index.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
- 
+
 import time
 import logging
 import os
+import json
 from kafka import KafkaProducer
 from kafka.errors import KafkaError
 
 logger = logging.getLogger('kafka')
 logger.setLevel(logging.INFO)
- 
+
 class KafkaToKafka(object):
     """
     kafka 消息投递 kafka
@@ -19,15 +20,15 @@ class KafkaToKafka(object):
         self.host = host
 
         self.producer = KafkaProducer(
-            bootstrap_servers = [self.host], 
-            retries = 10,
-            max_in_flight_requests_per_connection = 1, 
-            request_timeout_ms = 30000,
-            max_block_ms = 60000,
+            bootstrap_servers = [self.host],
+            # retries = 10,
+            # max_in_flight_requests_per_connection = 1, 
+            # request_timeout_ms = 30000,
+            # max_block_ms = 60000,
             **kwargs
         )
 
-    def send(self, topic, records):
+    def send(self, topic, event):
         """
         异步生产 kafka 消息
         """
@@ -43,17 +44,22 @@ class KafkaToKafka(object):
 
         s_time = time.time()
         try:
-            for record in records:
-                key = record["Ckafka"]["msgKey"]
-                # 当 key 为 "" 或者为 "None" 时，要传入key=None，这样python kafka库会随机选取一个partition写入消息
-                if key == "" or key == "None":
-                    key = None
-                value = record["Ckafka"]["msgBody"]
+            eventList = None
+            if "EventList" in event:
+                eventList = event["EventList"]
 
-                # 也可以对消息进行处理后再转存
-                #value = deal_message(record["Ckafka"]["msgBody"])
-
+            # peer
+            if eventList is None:
+                key = None
+                value = json.dumps(event)
                 self.producer.send(topic, key = key, value = value).add_callback(on_send_success).add_errback(on_send_error)
+            else:
+                # batch
+                for data in eventList:
+                    key = None
+                    value = json.dumps(data)
+                    self.producer.send(topic, key = key, value = value).add_callback(on_send_success).add_errback(on_send_error)
+
 
             # block until all async messages are sent
             self.producer.flush()
@@ -73,16 +79,16 @@ def deal_message(message):
 def main_handler(event, context):
     kafka_address = os.getenv("kafka_address")
     kafka_topic_name = os.getenv("kafka_topic_name")
-  
+
     kafka_to_kafka = KafkaToKafka(
-        kafka_address
-        #security_protocol = "SASL_PLAINTEXT",
+        kafka_address,
+        # security_protocol = "PLAINTEXT",
         #sasl_mechanism = "PLAIN",
         #sasl_plain_username = "ckafka-80o10xxx#lkoxx",
         #sasl_plain_password = "kongllxxxx",
-        #api_version=(0, 10, 2)
+        api_version=(0, 10, 2)
     )
 
-    ret = kafka_to_kafka.send(kafka_topic_name, event["Records"])
+    ret = kafka_to_kafka.send(kafka_topic_name, event)
     logger.info(ret)
     return ret
