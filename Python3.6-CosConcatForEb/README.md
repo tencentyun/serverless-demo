e2a48fedb9302a8df382b6fdb6f2c092bb8be687
rickcui <rickcui@tencent.com>
2021-11-08 10:26:24 +0800

    toCos 新增两个环境变量

--- Python3.6-CosConcatForEb/README.md
+++ Python3.6-CosConcatForEb/README.md
@@ -6,8 +6,11 @@
 2. 在`函数管理/函数配置/环境配置`设置环境变量
    + COS_REGION： 在`桶/概览/基本信息/所属地域`下 例如 `ap-chengdu`
       - COS_BUCKET_SRC和COS_BUCKET_DST都在同一个region下
+      - COS_BUCKET_SRC和COS_BUCKET_DST 可以是同一个bucket， 两个环境变量填相同的值即可
    + COS_BUCKET_SRC: 源bucket， 里面保存了`Python3.6-EventBridgeToCos`这个函数转储的文件
    + COS_BUCKET_DST: 目标bucket， 函数会将聚合后的文件保存到这个bucket里面
+   + MAX_FILE_SIZE_MB: 聚合后的文件最大体积限制(5<=size<=10000)。 单位：MB
+   + MAX_TIME_RANGE_HOURS: 聚合后单个文件内事件时间范围的最大值(1<=time<=24)。 单位:小时
 3. 在`函数管理/函数配置/环境配置`设置
    + 内存设置为1024M或更高
    + 执行超时时间设置为最大值，86400秒。 防止因为处理文件过多导致函数超时。
---
+++ Python3.6-CosConcatForEb/src/test_index.py
@@ -0,0 +1,133 @@
+from unittest import TestCase
+from index import split_group, parse_split_setting_time, parse_split_setting_size
+
+
+class TestParseSetting(TestCase):
+    def test_parse_split_setting_time(self):
+        self.assertEqual(parse_split_setting_time(None), None)
+        self.assertEqual(parse_split_setting_time(""), None)
+        self.assertRaisesRegex(ValueError, "时间范围错误", parse_split_setting_time, "0")
+        self.assertRaisesRegex(ValueError, "时间范围错误", parse_split_setting_time, "25")
+        self.assertEqual(parse_split_setting_time("1"), 3600)
+        self.assertEqual(parse_split_setting_time("24"), 86400)
+
+    def test_parse_split_setting_size(self):
+        self.assertEqual(parse_split_setting_size(None), None)
+        self.assertEqual(parse_split_setting_size(""), None)
+        self.assertRaisesRegex(ValueError, "文件大小错误", parse_split_setting_size, "4")
+        self.assertRaisesRegex(ValueError, "文件大小错误", parse_split_setting_size, "10001")
+        self.assertEqual(parse_split_setting_size("5"), 1048576*5)
+        self.assertEqual(parse_split_setting_size("10000"), 1048576*10000)
+
+
+class TestParseSplit(TestCase):
+    def test_split_group_time(self):
+        flist = [
+            ("1636297100.1.txt", 1),
+            ("1636297200.1.txt", 1),
+            ("1636297300.1.txt", 1),
+        ]
+        self.assertEqual(split_group(flist, 99, None), [
+            [('1636297100.1.txt', 1, 1636297100, '1636297100.1')],
+            [('1636297200.1.txt', 1, 1636297200, '1636297200.1')]
+        ])
+        self.assertEqual(split_group(flist, 100, None), [
+            [
+                ('1636297100.1.txt', 1, 1636297100, '1636297100.1'),
+                ('1636297200.1.txt', 1, 1636297200, '1636297200.1')
+            ]
+        ])
+        self.assertEqual(split_group(flist, 200, None), [
+            [
+                ('1636297100.1.txt', 1, 1636297100, '1636297100.1'),
+                ('1636297200.1.txt', 1, 1636297200, '1636297200.1'),
+                ('1636297300.1.txt', 1, 1636297300, '1636297300.1')
+            ]
+        ])
+        self.assertEqual(split_group(flist, 300, None), [])
+
+    def test_split_group_size(self):
+        flist1 = [
+            ("1636297100.1.txt", 1024),
+            ("1636297200.1.txt", 1024),
+            ("1636297300.1.txt", 1024),
+        ]
+        self.assertEqual(split_group(flist1, None, 1024), [
+            [('1636297100.1.txt', 1024, 1636297100, '1636297100.1')],
+            [('1636297200.1.txt', 1024, 1636297200, '1636297200.1')],
+            [('1636297300.1.txt', 1024, 1636297300, '1636297300.1')]
+        ])
+        self.assertEqual(split_group(flist1, None, 2048), [
+            [
+                ('1636297100.1.txt', 1024, 1636297100, '1636297100.1'),
+                ('1636297200.1.txt', 1024, 1636297200, '1636297200.1')
+            ]
+        ])
+        flist2 = [
+            ("1636297100.1.txt", 1024),
+            ("1636297200.1.txt", 1024),
+            ("1636297300.1.txt", 2048),
+        ]
+        self.assertEqual(split_group(flist2, None, 2048), [
+            [
+                ('1636297100.1.txt', 1024, 1636297100, '1636297100.1'),
+                ('1636297200.1.txt', 1024, 1636297200, '1636297200.1')
+            ],
+            [
+                ('1636297300.1.txt', 2048, 1636297300, '1636297300.1'),
+            ],
+        ])
+        self.assertEqual(split_group(flist2, None, 4096), [
+            [
+                ('1636297100.1.txt', 1024, 1636297100, '1636297100.1'),
+                ('1636297200.1.txt', 1024, 1636297200, '1636297200.1'),
+                ('1636297300.1.txt', 2048, 1636297300, '1636297300.1'),
+            ],
+        ])
+        self.assertEqual(split_group(flist2, None, 4097), [], "size不足， 返回空group")
+
+    def test_split_group_mix(self):
+        self.assertRaisesRegex(ValueError, "时间和文件大小至少需要设置一个", split_group, [], None, None)
+        self.assertRaisesRegex(ValueError, "时间和文件大小至少需要设置一个", split_group, [], "", "")
+        flist = [
+            ("1636297100.1.txt", 1024),
+            ("1636297200.1.txt", 1024),
+            ("1636297300.1.txt", 1024),
+            ("1636297400.1.txt", 1024),
+        ]
+        self.assertEqual(split_group(flist, 100, 2048), [
+            [
+                ('1636297100.1.txt', 1024, 1636297100, '1636297100.1'),
+                ('1636297200.1.txt', 1024, 1636297200, '1636297200.1')
+            ],
+            [
+                ('1636297300.1.txt', 1024, 1636297300, '1636297300.1'),
+                ('1636297400.1.txt', 1024, 1636297400, '1636297400.1')
+            ]
+        ], "time size同时满足")
+        self.assertEqual(split_group(flist, 200, 4096), [
+            [
+                ('1636297100.1.txt', 1024, 1636297100, '1636297100.1'),
+                ('1636297200.1.txt', 1024, 1636297200, '1636297200.1'),
+                ('1636297300.1.txt', 1024, 1636297300, '1636297300.1')
+            ]
+        ], "时间满足")
+        self.assertEqual(split_group(flist, 500, 2048), [
+            [
+                ('1636297100.1.txt', 1024, 1636297100, '1636297100.1'),
+                ('1636297200.1.txt', 1024, 1636297200, '1636297200.1')
+            ], [
+                ('1636297300.1.txt', 1024, 1636297300, '1636297300.1'),
+                ('1636297400.1.txt', 1024, 1636297400, '1636297400.1')
+            ]
+        ], "仅size满足1")
+        self.assertEqual(split_group(flist, 500, 3000), [
+            [
+                ('1636297100.1.txt', 1024, 1636297100, '1636297100.1'),
+                ('1636297200.1.txt', 1024, 1636297200, '1636297200.1')
+            ]
+        ], "仅size满足2")
+        self.assertEqual(split_group(flist, 1000, 5000), [], "所有条件都不满足")
+
+
+
