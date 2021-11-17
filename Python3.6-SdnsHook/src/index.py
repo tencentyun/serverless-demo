6b5d3744c0209e5c09e2ca19c4e2158b56c85db3
zrayzhou <zrayzhou@tencent.com>
2021-11-17 19:23:58 +0800

    sdnshook

---
+++ Python3.6-SdnsHook/src/index.py
@@ -0,0 +1,30 @@
+# -*- coding: utf8 -*-
+import json
+
+def main_handler(event, context):
+    response = {}
+
+    if "domainName" in event.keys():
+        response['domainName'] = event['domainName']
+    elif "clientIp" in event.keys():
+        response['clientIp'] = event['clientIp']
+    elif "location" in event.keys():
+        response['location'] = event['location']
+    elif "ttl" in event.keys():
+        response['ttl'] = event['ttl']
+    elif "ips" in event.keys():
+        response['ips'] = event['ips']
+    else:
+        return {"code": 410, "errorMsg": "param error"}
+
+    
+    if event['hookType'] == "BEFORE_WRITE_CACHE": 
+        print("BEFORE_WRITE_CACHE")
+    
+    if event['hookType'] == "BEFORE_WRITE_RESPONSE":  
+        print("BEFORE_WRITE_RESPONSE")
+    
+    response['ttl'] = 180
+
+    return(response)
+
