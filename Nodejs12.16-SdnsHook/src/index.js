8ff439469093b3eeeb185fd3ef32d229bd92217b
cocofhu <43171452+cocofhu@users.noreply.github.com>
2022-02-22 14:31:24 +0800

    Update index.js

--- Nodejs12.16-SdnsHook/src/index.js
+++ Nodejs12.16-SdnsHook/src/index.js
@@ -10,7 +10,9 @@ exports.main_handler = async (event, context, callback) => {
         ttl: ttl,
         ips: ips
     };
-    // 当解析一个不存在的域名或者解析失败时，底层将返回0或者空串
+    
+    // 当解析一个不存在的域名或者解析失败时，默认为0或空串，
+    // 而通过自定义解析功能，可将0排除，获取自定义的解析记录
     response.ips = response.ips.filter(ip=>ip!=0);
     
     if (hookType=="BEFORE_WRITE_CACHE") {
